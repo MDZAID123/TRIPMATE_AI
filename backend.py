d50db1483d018537dfd21566114f49e13d8e8c69
MDZAID123 <zaid20525@iiitd.ac.in>
2026-09-05 21:23:00 +0530

    fix groq model name and local ssl config

--- backend.py
+++ backend.py
@@ -52,7 +52,8 @@ if not GROQ_API_KEY:
 # =========================
 
 llm = ChatGroq(
-    model="llama-3.3-70b-versatile",
+    # model="llama-3.3-70b-versatile",
+    model="openai/gpt-oss-120b",
     api_key=GROQ_API_KEY
 )
 
